08a6e7d39cb39175b205adb3c43e0077f8131c28
Rafael Motta <rafaelmotta021@gmail.com>
2015-09-23 13:55:41 -0300

    Zipcode service

--- foodbox-api-client-admin.js
+++ foodbox-api-client-admin.js
@@ -1438,3 +1438,27 @@ var service = function service(Restangular) {
 };
 
 angular.module('foodbox.admin.api').factory('utilsApi', service);
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var service = function service(Restangular) {
+  return new ((function () {
+    function ZipcodeApi() {
+      _classCallCheck(this, ZipcodeApi);
+    }
+
+    _createClass(ZipcodeApi, [{
+      key: 'fetch',
+      value: function fetch(params) {
+        return Restangular.one('zipcode').get(params);
+      }
+    }]);
+
+    return ZipcodeApi;
+  })())();
+};
+
+angular.module('foodbox.admin.api').factory('zipcodeApi', service);
