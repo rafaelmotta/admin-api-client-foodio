3979eb3eb8a1b371efbae6fa5cbb52ce9a434f44
Rafael Motta <rafaelmotta021@gmail.com>
2016-01-12 14:02:09 -0200

    Corrige build do gulp

--- admin-api-client-foodio.js
+++ admin-api-client-foodio.js
@@ -991,6 +991,45 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var service = function service(Restangular, ApiBase) {
+
+  return new ((function (_ApiBase) {
+    _inherits(MetaTagsApi, _ApiBase);
+
+    function MetaTagsApi() {
+      _classCallCheck(this, MetaTagsApi);
+
+      _get(Object.getPrototypeOf(MetaTagsApi.prototype), 'constructor', this).apply(this, arguments);
+    }
+
+    _createClass(MetaTagsApi, [{
+      key: 'fetch',
+      value: function fetch() {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).one('meta_tags').get();
+      }
+    }, {
+      key: 'update',
+      value: function update(data) {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).one('meta_tags', data.id).patch({ meta_tag: data });
+      }
+    }]);
+
+    return MetaTagsApi;
+  })(ApiBase))();
+};
+
+service.$inject = ['Restangular', 'ApiBase'];
+angular.module('admin.api.client.foodio').factory('metaTagsApi', service);
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var service = function service($q, Restangular, ApiBase) {
 
   return new ((function (_ApiBase) {
