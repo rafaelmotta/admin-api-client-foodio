3d41b7479510ab5a6e6f2dc2f699a8e0b63d2fd9
Rafael Motta <rafaelmotta021@gmail.com>
2016-01-21 11:23:59 -0200

    Alterado update e create do product

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -10,30 +10,30 @@ let service = (Restangular, ApiBase, $q) => {
         .get();
     }
 
-    create(product) {
-      return this._serializeBeforeCreate(product).then((product) => {
-        if(angular.isArray(product.img) && product.img[0] || angular.isArray(product.img_hover) && product.img_hover[0]) {
-          return this.requestWithImage({
-            url: `companies/${this.company.id}/stores/${this.store.id}/products`,
-            method: 'POST',
-            data: product,
-            key: 'product',
-            imgKeys: ['img', 'img_hover'],
-            extraKeys: ['name', 'description', 'base_price']
-          });
-        } else {
-          return Restangular
-            .one('companies', this.company.id)
-            .one('stores', this.store.id)
-            .post('products', { product: product });
-        }
-      });
-    }
-
-    update(product) {
+    create(productCategory, productSubcategory, product) {
       if(angular.isArray(product.img) && product.img[0] || angular.isArray(product.img_hover) && product.img_hover[0]) {
         return this.requestWithImage({
-          url: `companies/${this.company.id}/stores/${this.store.id}/products/${product.id}`,
+          url: `companies/${this.company.id}/stores/${this.store.id}/product_categories/${productCategory.id}/product_subcategories/${productSubcategory.id}/products/${product.id}`,
+          method: 'POST',
+          data: product,
+          key: 'product',
+          imgKeys: ['img', 'img_hover'],
+          extraKeys: ['name', 'description', 'base_price']
+        });
+      } else {
+        return Restangular
+          .one('companies', this.company.id)
+          .one('stores', this.store.id)
+          .one('product_categories', productCategory.id)
+          .one('product_subcategories', productSubcategory.id)
+          .post('products', { product: product });
+      }
+    }
+
+    update(productCategory, productSubcategory, product) {
+      if(angular.isArray(product.img) && product.img[0] || angular.isArray(product.img_hover) && product.img_hover[0]) {
+        return this.requestWithImage({
+          url: `companies/${this.company.id}/stores/${this.store.id}/product_categories/${productCategory.id}/product_subcategories/${productSubcategory.id}/products/${product.id}`,
           method: 'PATCH',
           data: product,
           key: 'product',
@@ -44,20 +44,11 @@ let service = (Restangular, ApiBase, $q) => {
         return Restangular
           .one('companies', this.company.id)
           .one('stores', this.store.id)
+          .one('product_categories', productCategory.id)
+          .one('product_subcategories', productSubcategory.id)
           .one('products', product.id)
           .patch({ product: product });
       }
-    }
-
-    _serializeBeforeCreate(product) {
-      return $q((resolve, reject) => {
-        let data = angular.copy(product);
-
-        data.product_subcategory_id = data.category.id;
-        delete data.category;
-
-        resolve(data);
-      });
     }
   }
 };
