4fcef68e5fb471705f02ed40bdf56716e07b36b3
Rafael Motta <rafaelmotta021@gmail.com>
2016-02-25 17:12:23 -0300

    Rodado gulp

--- admin-api-client-foodio.js
+++ admin-api-client-foodio.js
@@ -1411,6 +1411,54 @@ function _inherits(subClass, superClass) { if (typeof superClass !== 'function' 
 
 var service = function service(Restangular, ApiBase, $q) {
   return new ((function (_ApiBase) {
+    _inherits(PrinterApi, _ApiBase);
+
+    function PrinterApi() {
+      _classCallCheck(this, PrinterApi);
+
+      _get(Object.getPrototypeOf(PrinterApi.prototype), 'constructor', this).apply(this, arguments);
+    }
+
+    _createClass(PrinterApi, [{
+      key: 'fetch',
+      value: function fetch() {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).one('printers').get();
+      }
+    }, {
+      key: 'create',
+      value: function create(printer) {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).post('printers', { printer: printer });
+      }
+    }, {
+      key: 'update',
+      value: function update(printer) {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).one('printers', printer.id).patch({ printer: printer });
+      }
+    }, {
+      key: 'destroy',
+      value: function destroy(printer) {
+        return Restangular.one('companies', this.company.id).one('stores', this.store.id).one('printers', printer.id).remove();
+      }
+    }]);
+
+    return PrinterApi;
+  })(ApiBase))();
+};
+
+service.$inject = ['Restangular', 'ApiBase', '$q'];
+angular.module('admin.api.client.foodio').factory('printerApi', service);
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var service = function service(Restangular, ApiBase, $q) {
+  return new ((function (_ApiBase) {
     _inherits(ProductApi, _ApiBase);
 
     function ProductApi() {
@@ -1433,7 +1481,7 @@ var service = function service(Restangular, ApiBase, $q) {
             method: 'POST',
             data: product,
             key: 'product',
-            imgKeys: ['img', 'img_hover', 'stores'],
+            imgKeys: ['img', 'img_hover'],
             extraKeys: ['name', 'description', 'order', 'base_price', 'stores', 'addon_categories']
           });
         } else {
@@ -1449,7 +1497,7 @@ var service = function service(Restangular, ApiBase, $q) {
             method: 'PATCH',
             data: product,
             key: 'product',
-            imgKeys: ['img', 'img_hover', 'stores'],
+            imgKeys: ['img', 'img_hover'],
             extraKeys: ['name', 'description', 'order', 'base_price', 'stores', 'addon_categories']
           });
         } else {
