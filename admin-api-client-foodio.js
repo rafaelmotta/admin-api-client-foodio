518489f36580c1ca9d874627497c6f80a1df80d3
Rafael Motta <rafaelmotta021@gmail.com>
2016-05-23 14:12:47 -0300

    cart item

--- admin-api-client-foodio.js
+++ admin-api-client-foodio.js
@@ -283,10 +283,25 @@ var service = function service($q, Restangular, $rootScope) {
       key: '_serialize',
       value: function _serialize(cartItem) {
         return new Promise(function (resolve, reject) {
+          var toPut = [];
+          var toRemove = [];
+
+          for (var i in cartItem.cart_item_addons_to_put) {
+            var addon = cartItem.cart_item_addons_to_put[i];
+            toPut.push({ id: addon.id });
+          }
+
+          for (var i in cartItem.cart_item_addons_to_remove) {
+            var addon = cartItem.cart_item_addons_to_remove[i];
+            toRemove.push({ id: addon.id });
+          }
+
           var data = {
             amount: cartItem.amount,
             note: cartItem.note,
-            product_id: cartItem.product.id
+            product_id: cartItem.product.id,
+            cart_item_addons_to_put_attributes: toPut,
+            cart_item_addons_to_remove_attributes: toRemove
           };
 
           resolve(data);
--- src/api/cartItemApi.js
+++ src/api/cartItemApi.js
@@ -40,10 +40,25 @@ let service = ($q, Restangular, $rootScope) => {
 
     _serialize(cartItem) {
       return new Promise((resolve, reject) => {
+        let toPut = [];
+        let toRemove = [];
+
+        for(let i in cartItem.cart_item_addons_to_put) {
+          let addon = cartItem.cart_item_addons_to_put[i];
+          toPut.push({ id: addon.id });
+        }
+
+        for(let i in cartItem.cart_item_addons_to_remove) {
+          let addon = cartItem.cart_item_addons_to_remove[i];
+          toRemove.push({ id: addon.id });
+        }
+        
         let data = {
           amount: cartItem.amount,
           note: cartItem.note,
           product_id: cartItem.product.id,
+          cart_item_addons_to_put_attributes: toPut,
+          cart_item_addons_to_remove_attributes: toRemove
         };
 
         resolve(data);
